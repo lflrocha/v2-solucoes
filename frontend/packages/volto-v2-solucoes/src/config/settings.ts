ad6ae01c35931be4268d9dd6e05f3fbd2cb4045f
Lincoln Araújo <lincoln.fa@hotmail.com>
2025-12-07 21:46:05 -0300

    ajuste nos pacotes frontend para plone 6

--- frontend/packages/volto-v2-solucoes/src/config/settings.ts
+++ frontend/packages/volto-v2-solucoes/src/config/settings.ts
@@ -1,10 +1,25 @@
-import type { ConfigType } from "@plone/registry";
+import type { ConfigType } from '@plone/registry';
 
 export default function install(config: ConfigType) {
-  // Language settings
   config.settings.isMultilingual = false;
-  config.settings.supportedLanguages = ["pt-br"];
-  config.settings.defaultLanguage = "pt-br";
+  config.settings.supportedLanguages = ['pt-br'];
+  config.settings.defaultLanguage = 'pt-br';
+
+  if (!config.settings.apiExpanders) {
+    config.settings.apiExpanders = [];
+  }
+
+  config.settings.apiExpanders = [
+    ...config.settings.apiExpanders,
+    {
+      match: '',
+      GET_CONTENT: ['inherit'],
+      querystring: {
+        'expand.inherit.behaviors':
+          'voltolighttheme.header,voltolighttheme.theme,voltolighttheme.footer',
+      },
+    },
+  ];
 
   return config;
 }
